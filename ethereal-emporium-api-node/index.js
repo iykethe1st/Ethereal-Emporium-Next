608004cfa305fc366e0578aa66803aadbfd12060
Ikenna Udemezue <iykethe1st@gmail.com>
2023-02-09 19:12:55 +0100

    added jwt/lodash/user registration

--- ethereal-emporium-api-node/index.js
+++ ethereal-emporium-api-node/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const users = require("./routes/users");
 const port = 3500;
 
 mongoose
   .connect("mongodb://localhost/ethereal-emporium")
   .then(() => console.log("Connected to MongoB..."))
   .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+app.use(express.json());
+
+app.use("/user", users);
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- ethereal-emporium-api-node/models/users.js
+++ ethereal-emporium-api-node/models/users.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const Joi = require("joi");
 const jwt = require("jsonwebtoken");
+const config = require("config");
 
 const userSchema = new Schema({
   name: {
